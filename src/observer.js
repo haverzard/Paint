6455a1aa87e2dcef80f8e7525437c166c7c5ad54
Yonatan <48679462+haverzard@users.noreply.github.com>
2021-02-06 14:46:20 +0700

    create simple view observer

---
+++ src/observer.js
@@ -0,0 +1,11 @@
+class Observer {
+    constructor() {
+        this.main = new MainView()
+        this.shadow = new ShadowView(this)    
+    }
+
+    putDrawing(buf, gl_mode) {
+        this.main.bank.addEntity(new Entity(this.main.getVertices().length/2, buf, gl_mode))
+        this.main.draw()
+    }
+}
