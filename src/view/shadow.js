ca8705a69e2869e5cf3c552c6cfb187451cdba68
BaconLover307 <kresnadi30@gmail.com>
2021-02-24 00:58:01 +0700

    Tidy up console log

--- src/view/shadow.js
+++ src/view/shadow.js
@@ -73,7 +73,7 @@ class ShadowView {
         const rect = canvas.getBoundingClientRect()
         const x = event.clientX - rect.left
         const y = event.clientY - rect.top
-        console.log('x: ' + x + ' y: ' + y)
+        // console.log('x: ' + x + ' y: ' + y)
 
         // hold press for cursor
         if (mode == MODE.CURSOR) {
